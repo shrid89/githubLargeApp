b78e5c4108ef5a0d5ea0308d45b33b561840848d
shrid89 <shrid89@users.noreply.github.com>
2016-02-08 18:53:59 +0530

    testing console added 1

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -20,6 +20,7 @@ $(function() {
     return false;
   });
   console.log("testzzzzzssssss");
+    console.log("testzzzzzssssaaass");
   $('#add-new-fact').click(function() {
     
     var name = $('#right-column h2').text();
